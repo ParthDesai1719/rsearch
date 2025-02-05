b21408ebb12171b8555b16faa4b46b07fb51fae2
Malhar Ujawane <malharujawane@gmail.com>
2025-02-06 00:53:42 +0530

    updated robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap-dev.xml`,
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap-daily.xml`,
   };
 }
